3a7ff5e50480013f16fc8cf80febd1299b2c3013
SYAFITO <Lenovo@DESKTOP-4CQ6R6H>
2025-11-24 09:13:43 +0700

    memperbaiki typo pada bagian connected

--- config/db.js
+++ config/db.js
@@ -3,13 +3,13 @@ const db = require('../models');
 async function connectDatabase() {
     try {
         await db.sequelize.authenticate();
-        console.log('Database connection successfully.');
-        
+        console.log('Database connected successfully.');
+
         await db.sequelize.sync({alter: true}); 
         console.log('Database synchronized.');
 
     }catch (error) {
-        console.error('Database connection failed:', error.message);
+        console.error('Database connection failed:', err.message);
         process
     }
 }
